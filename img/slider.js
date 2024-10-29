2488ce90168431685d86984530120bac8aca321e
Biswajeet Priyadarshi <143275912+Priyadarshi4n@users.noreply.github.com>
2024-10-29 12:18:18 +0530

    form js

--- img/slider.js
+++ img/slider.js
@@ -18,3 +18,25 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+
+window.addEventListener("load", function(){
+  setTimeout(
+    function open(event){
+      var popup = document.getElementById("popup");
+      popup.classList.toggle("active");
+      var blur = document.getElementById("blur");
+      blur.classList.toggle("active");
+    },
+  1000
+  )
+})
+
+function toggle() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup");
+  popup.classList.toggle("active");
+}
+
